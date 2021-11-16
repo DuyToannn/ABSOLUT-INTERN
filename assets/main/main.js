1507ceef46a53541670296be0495a7d2b72787a2
Duy Toan <toanpmo123@gmail.com>
2021-11-15 17:58:28 -0800

    up

--- assets/main/main.js
+++ assets/main/main.js
@@ -1,36 +1,123 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 6,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20
+    slidesPerView: 6,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "fraction",
     },
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 30
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    900: {
-      slidesPerView: 6,
-      spaceBetween: 40
-    },
-    1200: {
-      slidesPerView: 6,
-      spaceBetween: 40
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        900: {
+            slidesPerView: 6,
+            spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 6,
+            spaceBetween: 40
+        }
+
     }
-  
-  }
 });
-  
+
+var swiper = new Swiper(".featured__swiper", {
+    slidesPerView: 4,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+
+    }
+});
+
+var swiper = new Swiper(".latest__swiper", {
+    slidesPerView: 6,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+
+        },
+        900: {
+            slidesPerView: 6,
+            spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 6,
+            spaceBetween: 40
+        }
+
+    }
+});
+
+var swiper = new Swiper(".mixology__swiper", {
+    slidesPerView: 6,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+
+    }
+});
 $('.moreless-button').click(function() {
-  $('.moretext').toggle();
-  if ($('.moreless-button').text() == "SHOW MORE") {
-    $(this).text("SHOW LESS")
-  } else {
-    $(this).text("SHOW MORE")
-  }
+    $('.moretext').toggle();
+    if ($('.moreless-button').text() == "SHOW MORE") {
+        $(this).text("SHOW LESS")
+    } else {
+        $(this).text("SHOW MORE")
+    }
 });
